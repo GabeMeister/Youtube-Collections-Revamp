009c5892ff92058ff213dec4755029410fb30161
Gabe Jensen <gabe.jensen@wsu.edu>
2016-04-10 17:41:40 -0700

    Added "Mark as watched" functionality

--- YoutubeCollectionsRevamp/app/js/background.js
+++ YoutubeCollectionsRevamp/app/js/background.js
@@ -3,6 +3,9 @@ var queryInfo = {
 }
 var ytCollectionsUrl = 'http://localhost:5507/';
 
+var _hub = null;
+var _hubConnection = null;
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
@@ -35,6 +38,49 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
     }
 });
 
+chrome.runtime.onInstalled.addListener(function () {
+    var context = "link";
+    var title = "Mark video as watched";
+    var id = chrome.contextMenus.create({
+        "title": title,
+        "contexts": [context],
+        "id": "youtubeCollectionsRevampContextMenu"
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(markVideoAsWatched);
+
+function markVideoAsWatched(info, tab) {
+    var url = info.linkUrl;
+    
+    if (isYoutubeLink(url)) {
+        if (_hub === null) {
+            initializeHub();
+        }
+        
+        // Get just the video id
+        // Example url: https://www.youtube.com/watch?v=L0bF7Kj5rxI
+        // We want L0bF7Kj5rxI
+        var videoId = url.replace(/https:\/\/www.youtube.com\/watch\?v=/g, '');
+        var userYoutubeId = util.unquotify(localStorage.getItem('USER_YOUTUBE_ID'));
+        var dateViewed = formatDateTime(new Date());
+        
+		setTimeout(function() {
+			_hub.invoke('InsertWatchedVideo', videoId, userYoutubeId, dateViewed);
+		}, 1000);
+
+
+        // Remove video in view
+		chrome.tabs.query({ active: true, url: 'https://www.youtube.com/*' }, function (tabs) {
+		    
+		    chrome.tabs.sendMessage(tabs[0].id, { 'message': REMOVE_VIDEO, 'videoId': videoId });
+		    
+		});
+        
+    }
+
+}
+
 function fetchYoutubeId()
 {
     chrome.tabs.query(queryInfo, function (tabs) {
@@ -46,4 +92,41 @@ function fetchYoutubeId()
     });
 
     chrome.tabs.create({ url: 'https://www.youtube.com/' });
+}
+
+function isYoutubeLink(url) {
+    return url.indexOf("https://www.youtube.com/watch?v=") > -1;
+}
+
+function initializeHub() {
+
+    _hubConnection = $.hubConnection(HUB_SERVER_URL);
+    _hub = _hubConnection.createHubProxy('YoutubeCollectionsServer');
+
+    _hubConnection.start();
+
+}
+
+function formatDateTime(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var strTime = hours + ':' + minutes + ':' + seconds;
+
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    var strDate = year + '-' + month + '-' + day;
+
+    // We want YYYY-MM-DD hh:mm:ss format
+    return strDate + ' ' + strTime;
 }
--- YoutubeCollectionsRevamp/app/js/constants.js
+++ YoutubeCollectionsRevamp/app/js/constants.js
@@ -6,6 +6,7 @@ var HUB_SERVER_URL = 'http://localhost:3851';
 var FETCH_YOUTUBE_CHANNEL_ID_MSG = 'FETCH_YOUTUBE_CHANNEL_ID';
 var GET_CHANNEL_ID_MSG = 'GET_CHANNEL_ID';
 var NOTIFY_CHANNEL_ID_FOUND_MSG = 'NOTIFY_CHANNEL_ID_FOUND';
+var REMOVE_VIDEO = 'REMOVE_VIDEO';
 
 
 
@@ -29,7 +30,41 @@ var INITIALIZED = 'INITIALIZED';
 
 
 
-
+var RELATED_VIDEO_HTML_TEMPLATE = "<li class=\"video-list-item related-list-item show-video-time\"> " +
+"<div class=\"content-wrapper\"> <a href=\"/watch?v={{VideoID}}\" class=\" " +
+"content-link spf-link yt-uix-sessionlink\" title=\"{{VideoTitle}}\" rel=\"spf-prefetch\" " +
+"data-sessionlink=\"feature=related&amp;ei=15gdVe3RMs62-gOnqIHABA&amp;ved=CBcQzRooEg\">" +
+" <span dir=\"ltr\" class=\"title\" aria-describedby=\"description-id-251503\">" +
+" {{VideoTitle}} </span> <span class=\"accessible-description\" id=\"description-id-251503\">" +
+" - Duration: {{VideoTime}}. </span> <span class=\"stat attribution\"> <span class=\"" +
+"g-hovercard\" data-ytid=\"{{VideoChannelId}}\" data-name=\"related\"> by <span class=\"" +
+" g-hovercard\" data-ytid=\"{{VideoChannelId}}\" data-name=\"\">{{VideoChannel}}</span> " +
+"</span> </span> <span class=\"stat view-count\">{{VideoViews}}</span></a> </div><div " +
+"class=\"thumb-wrapper\"> <a href=\"/watch?v={{VideoID}}\" class=\" thumb-link spf-link" +
+" yt-uix-sessionlink\" tabindex=\"-1\" rel=\"spf-prefetch\" data-sessionlink=\"" +
+"feature=related&amp;ei=15gdVe3RMs62-gOnqIHABA&amp;ved=CBcQzRooEg\" aria-hidden=\"true\">" +
+"<span class=\"yt-uix-simple-thumb-wrap yt-uix-simple-thumb-related\" tabindex=\"0\" " +
+"data-vid=\"{{VideoID}}\"><img src=\"{{VideoThumbnail}}\" aria-hidden=\"true\" alt=\"\" " +
+"width=\"120\" height=\"90\" class=\"gif-delayer gif-delayer-loaded\"></span></a> " +
+"<span class=\"video-time\"> {{VideoTime}} </span> <span class=\"thumb-menu " +
+"dark-overflow-action-menu video-actions\"> <button class=\"yt-uix-button-reverse flip " +
+"addto-watch-queue-menu spf-nolink hide-until-delayloaded yt-uix-button yt-uix-button-dark-" +
+"overflow-action-menu yt-uix-button-size-default yt-uix-button-has-icon no-icon-markup " +
+"yt-uix-button-empty\" aria-expanded=\"false\" aria-haspopup=\"true\" type=\"button\" " +
+"onclick=\";return false;\"><span class=\"yt-uix-button-arrow yt-sprite\"></span><ul " +
+"class=\"watch-queue-thumb-menu yt-uix-button-menu yt-uix-button-menu-dark-overflow-action-menu " +
+"hid\"><li role=\"menuitem\" class=\"overflow-menu-choice addto-watch-queue-menu-choice " +
+"addto-watch-queue-play-next yt-uix-button-menu-item\" data-action=\"play-next\" onclick=\";" +
+"return false;\" data-video-ids=\"{{VideoID}}\"><span class=\"addto-watch-queue-menu-text\">" +
+"Play next</span></li><li role=\"menuitem\" class=\"overflow-menu-choice " +
+"addto-watch-queue-menu-choice addto-watch-queue-play-now yt-uix-button-menu-item\" " +
+"data-action=\"play-now\" onclick=\";return false;\" data-video-ids=\"{{VideoID}}\"><span " +
+"class=\"addto-watch-queue-menu-text\">Play now</span></li></ul></button> </span> <button" +
+" class=\"yt-uix-button yt-uix-button-size-small yt-uix-button-default yt-uix-button-empty " +
+"yt-uix-button-has-icon no-icon-markup addto-button addto-queue-button video-actions " +
+"spf-nolink hide-until-delayloaded addto-tv-queue-button yt-uix-tooltip\" type=\"button\" " +
+"onclick=\";return false;\" title=\"TV Queue\" data-video-ids=\"{{VideoID}}\" " +
+"data-style=\"tv-queue\"></button> </div></li>";
 
 
 
